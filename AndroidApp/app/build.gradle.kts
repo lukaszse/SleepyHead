d342501688540f71343743113a8aeab483e0fb26
lseremak <llseremak@gmail.com>
2026-03-10 23:15:58 +0100

    Polar H10 connectivity

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -38,14 +38,17 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // Kotlin Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     // Polar BLE SDK and RxJava
     implementation("com.github.polarofficial:polar-ble-sdk:5.5.0")
@@ -53,12 +56,12 @@ dependencies {
     implementation("io.reactivex.rxjava3:rxjava:3.1.8")
 
     // RxJava3 to Kotlin Coroutines Flow bridge
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx3:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-rx3:1.8.1")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
